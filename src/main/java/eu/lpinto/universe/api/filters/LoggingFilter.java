cc30817a83853e06f464baf2cd61617bc553c868
Luís Pinto <mail@lpinto.eu>
2017-07-08 18:02:00 +0300

    enable response logging

--- src/main/java/eu/lpinto/universe/api/filters/LoggingFilter.java
+++ src/main/java/eu/lpinto/universe/api/filters/LoggingFilter.java
@@ -49,20 +49,17 @@ public class LoggingFilter extends org.glassfish.jersey.filter.LoggingFilter {
      */
     @Override
     public void filter(final ContainerRequestContext requestContext, final ContainerResponseContext responseContext) throws IOException {
-        /*
-         *
-         * if ("GET".equals(requestContext.getMethod()) &&
-         * requestContext.getUriInfo().getAbsolutePath().toString().endsWith("api")) {
-         * return;
-         * }
-         *
-         * if ("GET".equals(requestContext.getMethod()) &&
-         * requestContext.getUriInfo().getAbsolutePath().toString().endsWith("api/")) {
-         * return;
-         * }
-         *
-         * super.filter(requestContext, responseContext);
-         *
-         */
+
+        if ("GET".equals(requestContext.getMethod())
+            && requestContext.getUriInfo().getAbsolutePath().toString().endsWith("api")) {
+            return;
+        }
+
+        if ("GET".equals(requestContext.getMethod())
+            && requestContext.getUriInfo().getAbsolutePath().toString().endsWith("api/")) {
+            return;
+        }
+
+        super.filter(requestContext, responseContext);
     }
 }
